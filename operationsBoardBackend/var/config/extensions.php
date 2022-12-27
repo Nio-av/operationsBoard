6d59327bb94e15e1bc5995b5863349e363473a27
Nicolas Tribukait <Nio-av@users.noreply.github.com>
2022-12-27 09:43:19 +0100

    install data hub

---
+++ operationsBoardBackend/var/config/extensions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "bundle" => [
+        "Pimcore\\Bundle\\DataHubBundle\\PimcoreDataHubBundle" => [
+            "enabled" => TRUE,
+            "priority" => 11,
+            "environments" => [
+
+            ]
+        ]
+    ]
+];
